0a2008d734344655a5e491e29fc3b44bae106f5a
Juno <kjo1797@gmail.com>
2023-08-26 18:47:44 +0900

    picnic in algospot (exhaustive search, brute force)

---
+++ picnic.cpp
@@ -0,0 +1,50 @@
+// https://www.algospot.com/judge/problem/read/PICNIC
+// exhaustive search
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int c, n, m;
+bool is_friend[10][10];
+
+int count_pairs(bool picked[10]) {
+    int first = -1;
+    for(int i = 0; i < n; i++) {
+        if(!picked[i]) {
+            first = i;
+            break;
+        }
+    }
+    if(first == -1) return 1;
+
+    int ret = 0;
+    for(int i = first + 1; i < n; i++) {
+        if(!picked[i] && is_friend[first][i]) {
+            picked[first] = picked[i] = true;
+            ret += count_pairs(picked);
+            picked[first] = picked[i] = false;
+        }
+    }
+    return ret;
+}
+
+int main() {
+    int a, b;
+    cin >> c;
+    for(int t = 0; t < c; t++) {
+        cin >> n >> m;
+        bool picked[10] = {false,};
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                is_friend[i][j] = false;
+            }
+        }
+        for(int i = 0; i < m; i++) {
+            cin >> a >> b;
+            is_friend[a][b] = true;
+            is_friend[b][a] = true;
+        }
+        cout << count_pairs(picked) << '\n';
+    }
+}
