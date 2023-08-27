d30deea8a4c7de198370151cec9f4849fd804c20
KIMJunOh <kjo1797@gmail.com>
2023-08-27 19:46:19 +0900

    picnic in algospot(exhaustive search, brute force)

--- algospot/picnic.cpp
+++ algospot/picnic.cpp
@@ -9,6 +9,7 @@ int c, n, m;
 bool is_friend[10][10];
 
 int count_pairs(bool picked[10]) {
+    // 짝이 없는 첫 번째 사람을 찾는다 (오름차순으로만 짝을 지어 중복 count 방지)
     int first = -1;
     for(int i = 0; i < n; i++) {
         if(!picked[i]) {
