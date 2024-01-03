713d71db244c4fa90afe86f1aac882dfbba31a00
Juno <kjo1797@gmail.com>
2024-01-03 13:27:26 +0900

    1068 (tree) refactored

--- 1068.py
+++ 1068.py
@@ -5,19 +5,17 @@ deleted = int(input())
 is_leaf = [True for _ in range(n)]
 is_leaf[deleted] = False
 for i in range(n):
-    now = i
-    p = parent_of[now]
+    p = parent_of[i]
     # skip if already checked
-    if not is_leaf[now]:
+    if not is_leaf[i]:
         continue
     # check all ancestors as False
     # until the root or the deleted node
     while p != -1 and p != deleted:
         is_leaf[p] = False
-        parent_of[now] = parent_of[p]
         p = parent_of[p]
     # if the deleted node is the one of ancestors, this node is also deleted
     if p == deleted:
-        is_leaf[now] = False
+        is_leaf[i] = False
 
 print(sum(is_leaf))
